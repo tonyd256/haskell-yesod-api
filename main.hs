cfc81067e5492a36aa989cc07cb246e8bbf3610c
Tony DiPasquale <tony@thoughtbot.com>
2015-11-20 14:49:28 -0800

    Rename app

--- main.hs
+++ main.hs
@@ -7,19 +7,19 @@ import Yesod.Core
 import Network.Wai.Handler.Warp
 import Data.Streaming.Network.Internal
 
-data HelloWorld = HelloWorld
+data YesodAPI = YesodAPI
 
-mkYesod "HelloWorld" [parseRoutes|
+mkYesod "YesodAPI" [parseRoutes|
 / HomeR GET
 |]
 
-instance Yesod HelloWorld
+instance Yesod YesodAPI
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout [whamlet|Hello World!|]
 
 main :: IO ()
-main = warp' 3000 "0.0.0.0" HelloWorld
+main = warp' 3000 "0.0.0.0" YesodAPI
 
 warp' :: YesodDispatch site => Int -> String -> site -> IO ()
 warp' port host site = do
